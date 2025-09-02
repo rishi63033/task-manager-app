93549e1e1089b8e4f1c0306f9c289141cfbb1fd8
enjam rishikesh reddy <97114842+rishi63033@users.noreply.github.com>
2025-09-02 14:43:57 +0530

    Update firebaseAuth.js

--- middleware/firebaseAuth.js
+++ middleware/firebaseAuth.js
@@ -1,5 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')
+);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
